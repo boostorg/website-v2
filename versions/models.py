13d8a67fb681924cc8e81c0552f3441524334ec8
Frank Wiles <frank@revsys.com>
2021-12-20 13:36:06 -0600

    Fix failing test

--- versions/models.py
+++ versions/models.py
@@ -5,21 +5,24 @@ from django.db import models
 
 
 class VersionFile(models.Model):
-    Unix = 'Unix'
-    Windows = 'Windows'
+    Unix = "Unix"
+    Windows = "Windows"
     OPERATING_SYSTEM_CHOICES = (
-        (Unix, 'Unix'),
-        (Windows, 'Windows'),
+        (Unix, "Unix"),
+        (Windows, "Windows"),
     )
     checksum = models.CharField(max_length=64, unique=True, default=None)
     file = models.FileField(upload_to="uploads/")
     operating_system = models.CharField(
-        choices=OPERATING_SYSTEM_CHOICES, max_length=15, null=False, blank=False)
-    
+        choices=OPERATING_SYSTEM_CHOICES, max_length=15, null=False, blank=False
+    )
+
     def save(self, *args, **kwargs):
         if self.file is not None:
             if self.checksum is None:
-                self.checksum = hashlib.sha256(self.file.name.encode("utf-8")).hexdigest()
+                self.checksum = hashlib.sha256(
+                    self.file.name.encode("utf-8")
+                ).hexdigest()
             super().save(*args, **kwargs)
 
 
