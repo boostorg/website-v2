8499d3aec004665dc5aa597eb31f00dfc7308396
Greg Newman <greg@gregnewman.org>
2022-05-12 08:45:59 -0400

    parse markdown and frontmatter

---
+++ core/markdown.py
@@ -0,0 +1,15 @@
+import frontmatter
+from mistletoe import Document, HTMLRenderer
+
+
+def process_md():
+    with open('content/test.md') as f:
+        post = frontmatter.load(f)
+        metadata = post.metadata
+        content = post.content
+
+        with HTMLRenderer() as renderer:
+            doc = Document(content)
+            rendered = renderer.render(doc)
+
+    return metadata, rendered
