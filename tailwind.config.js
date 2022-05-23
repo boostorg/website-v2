478a22028f4db18da246a35aacad76df6eba2947
Greg Newman <greg@gregnewman.org>
2022-05-23 17:54:01 -0400

    Colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ["templates/**/*.html"],
   darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      'charcoal': '#172A34',
+      'orange': '#FF9F00',
+      'green': '#5AD599',
+      'black': '#051A26',
+      'slate': '#314A57',
+      'steel': '#B5C9D3',
+      'stone-white': '#DDE7EC',
+    },
+    extend: {
+      fontFamily: {
+        cairo: "'Cairo', sans-serif",
+      },
+    },
   },
   variants: {
     extend: {},
