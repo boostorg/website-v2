47f0c7cf714c2afd17e68e96f0f87dec67727476
Frank Wiles <frank@revsys.com>
2022-05-26 08:33:31 -0500

    Black codebase

--- core/shortcode_token.py
+++ core/shortcode_token.py
@@ -2,10 +2,10 @@ import re
 import mistletoe.span_token as span_token
 
 
-__all__ = ['Math']
+__all__ = ["Math"]
 
 
 class Math(span_token.SpanToken):
-    pattern = re.compile(r'(\${1,2})([^$]+?)\1')
+    pattern = re.compile(r"(\${1,2})([^$]+?)\1")
     parse_inner = False
     parse_group = 0
