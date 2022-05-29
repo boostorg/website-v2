aa7a55df2c484516ec32e7ac98809daf56da524c
Frank Wiles <frank@revsys.com>
2022-05-29 10:14:29 -0500

    Additional Version API test
    - Added example/test archive files in various formats
    - Fix permission issue for public vs contributor

--- versions/tests/test_models.py
+++ versions/tests/test_models.py
@@ -4,3 +4,7 @@ import datetime
 def test_version_creation(version):
     today = datetime.date.today()
     assert version.release_date < today
+
+
+def test_version_file_creation(full_version_one):
+    assert full_version_one.files.count() == 3
