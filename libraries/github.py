830bec82427bac439e499401fecc63f55b3b3755
Frank Wiles <frank@revsys.com>
2022-07-24 14:18:24 -0500

    Finished Github parsing code with exclusions

--- libraries/github.py
+++ libraries/github.py
@@ -6,7 +6,7 @@ import requests
 import structlog
 
 from ghapi.all import GhApi, paged
-from .models import Library
+from .models import Library, Category
 
 logger = structlog.get_logger()
 
@@ -79,7 +79,6 @@ def parse_submodules(content):
     url_re = re.compile(r"^\s*url\s*\=\s*\.\.\/(.*)\.git\s*$")
 
     for line in content.split("\n"):
-        print(line)
         sub_m = submodule_re.match(line)
         if sub_m:
             current_submodule = {"module": sub_m.group(1)}
@@ -89,7 +88,6 @@ def parse_submodules(content):
         if url_m:
             name = url_m.group(1)
             current_submodule["url"] = name
-            print(f"FOUND: {current_submodule}")
             modules.append(current_submodule)
             current_submodule = None
 
@@ -105,6 +103,17 @@ class LibraryUpdater:
     def __init__(self, owner="boostorg"):
         self.api = get_api()
         self.owner = owner
+        self.logger = structlog.get_logger()
+
+        # Modules we need to skip as they are not really Boost Libraries
+        self.skip_modules = [
+            "inspect",
+            "boostbook",
+            "bcp",
+            "build",
+            "quickbook",
+            "litre",
+        ]
 
     def get_ref(self, repo, ref):
         """Get a particular ref of a particular repo"""
@@ -115,7 +124,11 @@ class LibraryUpdater:
         return self.get_ref(repo="boost", ref="heads/master")
 
     def get_library_list(self):
-        """Determine our list of libraries"""
+        """
+        Determine our list of libraries from .gitmodules and sub-repo
+        libraries.json files
+        """
+        # Find our latest .gitmodules
         r = self.get_boost_ref()
         tree_sha = r["object"]["sha"]
 
@@ -132,7 +145,51 @@ class LibraryUpdater:
             gitmodules = base64.b64decode(f["content"])
             break
 
-        modules = parse_submodules(gitmodules)
+        modules = parse_submodules(gitmodules.decode("utf-8"))
+
+        # Parse the modules into libraries.  Most libraries are individual
+        # repositories, but a few such as "system", "functional", and others
+        # contain multiple libraries
+        libraries = []
+        for m in modules:
+            name = m["module"]
+
+            if name in self.skip_modules:
+                self.logger.info("skipping_library", skipped_library=name)
+                continue
+
+            meta = self.get_library_metadata(repo=name)
+            github_url = f"https://github.com/boostorg/{name}/"
+            if type(meta) is list:
+                for sublibrary in meta:
+                    libraries.append(
+                        {
+                            "name": name,
+                            "github_url": github_url,
+                            "authors": sublibrary["authors"],
+                            "description": sublibrary["description"],
+                            "category": sublibrary["category"],
+                            "authors": sublibrary["authors"],
+                            "maintainers": sublibrary.get("maintainers", []),
+                            "cxxstd": sublibrary.get("cxxstd"),
+                        }
+                    )
+
+            elif type(meta) is dict:
+                libraries.append(
+                    {
+                        "name": name,
+                        "github_url": github_url,
+                        "authors": meta["authors"],
+                        "description": meta["description"],
+                        "category": meta["category"],
+                        "authors": meta["authors"],
+                        "maintainers": meta.get("maintainers", []),
+                        "cxxstd": meta.get("cxxstd"),
+                    }
+                )
+
+        return libraries
 
     def get_library_metadata(self, repo):
         """Retrieve library metadata from 'meta/libraries.json'"""
@@ -141,14 +198,14 @@ class LibraryUpdater:
             r = requests.get(url)
             return r.json()
         except Exception as e:
-            logger.exception("get_library_metadata_failed", repo=repo, url=url)
+            self.logger.exception("get_library_metadata_failed", repo=repo, url=url)
             return None
 
     def update_libraries(self):
         """Update all libraries and they metadata"""
         libs = self.get_library_list()
 
-        logger.info("update_all_libraries_metadata", library_count=len(libs))
+        self.logger.info("update_all_libraries_metadata", library_count=len(libs))
 
         for lib in libs:
             self.update_library(lib)
@@ -162,21 +219,18 @@ class LibraryUpdater:
 
     def update_library(self, lib):
         """Update an individual library"""
-        logger = logger.bind(lib=lib)
+        logger = self.logger.bind(lib=lib)
         try:
-            obj, created = Library.objects_get_or_create(name=lib)
-            logger = logger.bind(created=created)
+            obj, created = Library.objects.get_or_create(name=lib["name"])
+            obj.github_url = lib["github_url"]
+            obj.description = lib["description"]
 
-            meta = self.get_library_metadata(lib)
-            if meta is not None:
-                logger = logger.bind(meta=meta)
-                obj.description = meta["description"]
+            # Update categories
+            self.update_categories(obj, categories=lib["category"])
 
-                # Update categories
-                self.update_categories(obj, categories=meta["category"])
-
-                # Save any changes
-                obj.save()
+            # Save any changes
+            logger = logger.bind(obj_created=created)
+            obj.save()
 
             logger.info("library_udpated")
 
