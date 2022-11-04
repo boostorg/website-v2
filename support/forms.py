6a9a0edbbb944939e0299aa365e7d19cdab5b458
Greg Newman <greg@gregnewman.org>
2022-11-04 10:00:26 -0400

    :construction: WIP support app

---
+++ support/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class ContactForm(forms.Form):
+
+    TOPIC_CHOICES = (
+        ("Sponsorship", "sponsorship"),
+        ("Security", "security"),
+        ("General", "general"),
+    )
+
+    first_name = forms.CharField(label=_("first name"), max_length=50)
+    last_name = forms.CharField(label=_("last name"), max_length=50)
+    email = forms.CharField(label=_("email"), max_length=150)
+    message = forms.CharField(label=_("message"), widget=forms.Textarea)
+    phone_number = forms.CharField(label=_("phone number"), max_length=30, required=False)
+    topic = forms.ChoiceField(label="topic", required=False, choices=TOPIC_CHOICES)
---
+++ support/views.py
@@ -0,0 +1,30 @@
+from django.views.generic import TemplateView
+from .forms import ContactForm
+
+
+class SupportBaseView(TemplateView):
+    """
+    Generic base view for the contact form
+    used on contact and support views
+    """
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = ContactForm
+        return context
+
+
+class SupportView(SupportBaseView):
+    """
+    View for the Support page with contact form
+    TODO: Add reCaptcha
+    """
+    template_name = "support/support.html"
+
+
+class ContactView(SupportBaseView):
+    """
+    View for the Support page with contact form
+    TODO: Add reCaptcha
+    """
+    template_name = "support/contact.html"
