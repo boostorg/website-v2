e74f3495c816453d701335a33cd37a1f3b4f08bf
Greg Newman <greg@gregnewman.org>
2022-11-04 10:03:25 -0400

    blacken

--- support/forms.py
+++ support/forms.py
@@ -14,5 +14,7 @@ class ContactForm(forms.Form):
     last_name = forms.CharField(label=_("last name"), max_length=50)
     email = forms.CharField(label=_("email"), max_length=150)
     message = forms.CharField(label=_("message"), widget=forms.Textarea)
-    phone_number = forms.CharField(label=_("phone number"), max_length=30, required=False)
+    phone_number = forms.CharField(
+        label=_("phone number"), max_length=30, required=False
+    )
     topic = forms.ChoiceField(label="topic", required=False, choices=TOPIC_CHOICES)
