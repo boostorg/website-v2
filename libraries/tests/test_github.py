307d587e65a6e57f24db0e0ab60aab0240b29a4c
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-11-30 14:57:12 -0800

    :umbrella: Add tests of library importer

---
+++ libraries/tests/test_github.py
@@ -0,0 +1,101 @@
+import responses
+from unittest.mock import patch
+
+from ghapi.all import GhApi
+
+from libraries.github import LibraryUpdater
+from libraries.github import get_api
+from libraries.models import Library
+
+
+def test_get_api():
+    result = get_api()
+    assert isinstance(result, GhApi)
+
+
+# LibraryUpdater tests
+
+
+def test_get_ref(github_api_get_ref_response):
+    """LibraryUpdater.get_ref()"""
+    with patch("libraries.github.LibraryUpdater.get_ref") as get_ref_mock:
+        updater = LibraryUpdater()
+        get_ref_mock.return_value = github_api_get_ref_response
+        result = updater.get_ref(repo="boost", ref="heads/master")
+        assert "object" in result
+
+
+def test_get_boost_ref(tp, github_api_get_ref_response):
+    """LibraryUpdater.get_boost_ref()"""
+    with patch("libraries.github.LibraryUpdater.get_ref") as get_ref_mock:
+        updater = LibraryUpdater()
+        get_ref_mock.return_value = github_api_get_ref_response
+        result = updater.get_boost_ref()
+        assert "object" in result
+        assert "url" in result
+        assert "boostorg" in result["url"]
+
+
+def test_get_library_list(github_api_get_ref_response):
+    """LibraryUpdater.get_library_list()"""
+    updater = LibraryUpdater()
+    result = updater.get_library_list()
+    # breakpoint()
+    pass
+
+
+@responses.activate
+def test_get_library_metadata(library_metadata):
+    """LibraryUpdater.get_library_metadata()"""
+    repo = "rational"
+    url = (
+        f"https://raw.githubusercontent.com/boostorg/{repo}/develop/meta/libraries.json"
+    )
+    responses.add(responses.GET, url, json=library_metadata)
+    updater = LibraryUpdater()
+    result = updater.get_library_metadata(repo)
+    assert result == library_metadata
+
+
+def test_update_libraries(github_library):
+    assert Library.objects.count() == 0
+
+    with patch(
+        "libraries.github.LibraryUpdater.get_library_list"
+    ) as get_library_list_mock:
+        get_library_list_mock.return_value = [github_library]
+        updater = LibraryUpdater()
+        updater.update_libraries()
+
+    assert Library.objects.filter(name=github_library["name"]).exists()
+    library = Library.objects.get(name=github_library["name"])
+    assert library.github_url == github_library["github_url"]
+    assert library.description == github_library["description"]
+    assert library.cpp_standard_minimum == github_library["cxxstd"]
+    assert library.categories.filter(name="sample1").exists()
+    assert library.categories.filter(name="sample2").exists()
+
+
+def test_update_library(github_library):
+    """LibraryUpdater.update_library()"""
+    assert Library.objects.count() == 0
+    updater = LibraryUpdater()
+    updater.update_library(github_library)
+    assert Library.objects.filter(name=github_library["name"]).exists()
+    library = Library.objects.get(name=github_library["name"])
+    assert library.github_url == github_library["github_url"]
+    assert library.description == github_library["description"]
+    assert library.cpp_standard_minimum == github_library["cxxstd"]
+    assert library.categories.filter(name="sample1").exists()
+    assert library.categories.filter(name="sample2").exists()
+
+
+def test_update_categories(library):
+    """LibraryUpdater.update_categories()"""
+    assert library.categories.count() == 0
+    updater = LibraryUpdater()
+    updater.update_categories(library, ["sample1", "sample2"])
+    library.refresh_from_db()
+    assert library.categories.count() == 2
+    assert library.categories.filter(name="sample1").exists()
+    assert library.categories.filter(name="sample2").exists()
