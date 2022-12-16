9ca553d5a2b6f4928225ff328fbfcc4d1ba5fafa
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-12-15 16:02:50 -0800

    :books: Update docstring

--- libraries/management/commands/update_libraries.py
+++ libraries/management/commands/update_libraries.py
@@ -5,5 +5,11 @@ from libraries.github import LibraryUpdater
 
 @click.command()
 def command():
+    """
+    Calls the LibraryUpdater, which retrieves the active boost libraries
+    from the Boost repo and updates the models in our database with the latest
+    information on that library (repo) and its issues, pull requests, and related
+    objects from GitHub.
+    """
     updater = LibraryUpdater()
     updater.update_libraries()
