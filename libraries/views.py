9e4b77f4a32de377df9a0bbe6e12680c5d3a33e6
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-02-09 10:21:36 -0800

    :fire: Remove the libraries-by-letter view

--- libraries/views.py
+++ libraries/views.py
@@ -38,22 +38,6 @@ class LibraryList(CategoryMixin, FormMixin, ListView):
         return super().get(request)
 
 
-class LibraryByLetter(CategoryMixin, ListView):
-    """List all of our libraries that begin with a certain letter"""
-
-    paginate_by = 25
-    template_name = "libraries/list.html"
-
-    def get_queryset(self):
-        letter = self.kwargs.get("letter")
-
-        return (
-            Library.objects.prefetch_related("categories")
-            .filter(name__startswith=letter.lower())
-            .order_by("name")
-        )
-
-
 class LibraryByCategory(CategoryMixin, ListView):
     """List all of our libraries in a certain category"""
 
