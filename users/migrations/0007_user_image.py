59a72c9a9a773b76e1fb64c744812c70b031a770
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-02-15 14:44:28 -0800

    :shirt: Linter

--- users/migrations/0007_user_image.py
+++ users/migrations/0007_user_image.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0006_auto_20220209_1545'),
+        ("users", "0006_auto_20220209_1545"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='image',
-            field=models.FileField(blank=True, null=True, upload_to='profile-images'),
+            model_name="user",
+            name="image",
+            field=models.FileField(blank=True, null=True, upload_to="profile-images"),
         ),
     ]
