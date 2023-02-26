fcb20db5716bbecc6b6b2d2b8f322283245bba75
Frank Wiles <frank@revsys.com>
2023-02-26 14:48:47 -0600

    Setup django-storages for S3 compatible storage when deployed

---
+++ core/storages.py
@@ -0,0 +1,8 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class MediaStorage(S3Boto3Storage):
+    bucket_name = settings.MEDIA_BUCKET_NAME
+    default_acl = "public-read"
+    file_overwrite = True
+    custom_domain = False
