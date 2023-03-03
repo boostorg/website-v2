a1818c6bfdae09f2b375c78d3c02ef8ec9ac0882
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-03-03 13:48:44 -0800

    :wrench: Reorder functions

--- libraries/utils.py
+++ libraries/utils.py
@@ -3,19 +3,14 @@ import structlog
 
 from dateutil.parser import ParserError, parse
 
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from django.utils.text import slugify
 
 logger = structlog.get_logger()
 
-
-def parse_date(date_str):
-    """Parses a date string to a datetime. Does not return an error."""
-    try:
-        return parse(date_str)
-    except ParserError:
-        logger.info("parse_date_invalid_date", date_str=date_str)
-        return None
+User = get_user_model()
 
 
 def extract_email(val: str) -> str:
@@ -41,3 +36,12 @@ def extract_email(val: str) -> str:
             logger.info("Could not extract valid email", value=val)
             return
         return email
+
+
+def parse_date(date_str):
+    """Parses a date string to a datetime. Does not return an error."""
+    try:
+        return parse(date_str)
+    except ParserError:
+        logger.info("parse_date_invalid_date", date_str=date_str)
+        return None
