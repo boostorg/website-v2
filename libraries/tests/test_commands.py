d32f3db121747a1262c7fca4ebee4688b19deaf9
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-03-08 14:16:25 -0800

    :sparkles: Function to extract and reformat email address

--- libraries/tests/test_commands.py
+++ libraries/tests/test_commands.py
@@ -1,4 +1,8 @@
-from ..management.commands.load_contributors import extract_names, generate_fake_email
+from ..management.commands.load_contributors import (
+    extract_email,
+    extract_names,
+    generate_fake_email,
+)
 
 
 def test_extract_names():
@@ -29,3 +33,21 @@ def test_generate_fake_email():
     result = generate_fake_email(sample)
     assert expected in result
     assert "@example.com" in result
+
+
+def test_extract_email():
+    expected = "t_testerson@example.com"
+    result = extract_email("Tester Testerston <t_testerson -at- example.com>")
+    assert expected == result
+
+    expected = "t.t.testerson@example.com"
+    result = extract_email("Tester Testerston <t.t.testerson -at- example.com>")
+    assert expected == result
+
+    expected = "t.t.testerson@example.sample.com"
+    result = extract_email("Tester Testerston <t.t.testerson -at- example.sample.com> ")
+    assert expected == result
+
+    expected = None
+    result = extract_email("Tester Testeron")
+    assert expected == result
