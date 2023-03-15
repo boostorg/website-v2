ea66abe921c3bd7e2859b3fab7c86c8efd6e7342
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-03-15 12:31:45 -0700

    :pencil: Match contributors by name when uploading
    Reduces the duplicates by a lot. Some still remain, mostly related to differences in spelling (Jeff versus Jeffrey) or differences in diacritical marks.

--- users/models.py
+++ users/models.py
@@ -65,6 +65,48 @@ class UserManager(BaseUserManager):
         logger.info("Creating stub user with email='%s'", email)
         return self._create_user(email, password, claimed=claimed, **extra_fields)
 
+    def find_user(
+        self, email: str = None, first_name: str = None, last_name: str = None
+    ):
+        """
+        Lazily finds a matching User record by email, or first name and last name.
+
+        This method is intended to be used when uploading library contributors in situations where we don't have the
+        contributor's email address. It first checks if a user with the given email exists, and if found, returns that
+        user. If no user is found with the given email, it checks if a user with the given first name and last name
+        exists, and returns that user if found. Otherwise, it returns None.
+
+        Args:
+            email (str): The email address of the user to find.
+            first_name (str): The first name of the user to find.
+            last_name (str): The last name of the user to find.
+
+        Returns:
+            A User object if a matching record is found, otherwise None.
+        """
+        # Check if a user with the email exists
+        user = self.model.objects.filter(email=email).first()
+
+        # If a user with the email is found, return that user
+        if user:
+            return user
+
+        # If not found, check for a user with the same first and last name
+        if self.model.objects.filter(
+            first_name=first_name, last_name=last_name
+        ).exists():
+            try:
+                return self.model.objects.get(
+                    first_name=first_name, last_name=last_name
+                )
+            except self.model.MultipleObjectsReturned:
+                logger.info(
+                    "Multiple users found with names",
+                    first_name=first_name,
+                    last_name=last_name,
+                )
+                return
+
     def record_login(self, user=None, email=None):
         """
         Record a succesful login to last_login for the user by user
