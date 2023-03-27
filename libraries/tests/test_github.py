87cb14e991427b28abf66dd2898d66a6a611ab91
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-03-27 12:08:52 -0700

    :wrench: Refactor contributor methods into GitHub processes

--- libraries/tests/test_github.py
+++ libraries/tests/test_github.py
@@ -199,6 +199,93 @@ def test_parse_libraries_json():
     }
 
 
+def test_extract_names():
+    sample = "Tester Testerson <tester -at- gmail.com>"
+    expected = ["Tester", "Testerson"]
+    result = GithubDataParser().extract_names(sample)
+    assert expected == result
+
+    sample = "Tester Testerson"
+    expected = ["Tester", "Testerson"]
+    result = GithubDataParser().extract_names(sample)
+    assert expected == result
+
+    sample = "Tester de Testerson <tester -at- gmail.com>"
+    expected = ["Tester de", "Testerson"]
+    result = GithubDataParser().extract_names(sample)
+    assert expected == result
+
+    sample = "Tester de Testerson"
+    expected = ["Tester de", "Testerson"]
+    result = GithubDataParser().extract_names(sample)
+    assert expected == result
+
+    sample = "Various"
+    expected = ["Various", ""]
+    result = GithubDataParser().extract_names(sample)
+    assert expected == result
+
+
+def test_extract_email():
+    expected = "t_testerson@example.com"
+    result = GithubDataParser().extract_email(
+        "Tester Testerston <t_testerson -at- example.com>"
+    )
+    assert expected == result
+
+    expected = "t.t.testerson@example.com"
+    result = GithubDataParser().extract_email(
+        "Tester Testerston <t.t.testerson -at- example.com>"
+    )
+    assert expected == result
+
+    expected = "t.t.testerson@example.sample.com"
+    result = GithubDataParser().extract_email(
+        "Tester Testerston <t.t.testerson -at- example.sample.com> "
+    )
+    assert expected == result
+
+    expected = None
+    result = GithubDataParser().extract_email("Tester Testeron")
+    assert expected == result
+
+    expected = "t_tester@example.com"
+    result = GithubDataParser().extract_email(
+        "Tester Testerston <t -underscore- tester -at- example -dot- com> "
+    )
+    assert expected == result
+
+    expected = "tester@example.com"
+    result = GithubDataParser().extract_email(
+        "Tester Testerston <tester - at - example.com> "
+    )
+    assert expected == result
+
+
+def test_extract_contributor_data():
+    sample = "Tester Testerson <tester -at- gmail.com>"
+    expected = {
+        "valid_email": True,
+        "email": "tester@gmail.com",
+        "first_name": "Tester",
+        "last_name": "Testerson",
+    }
+    result = GithubDataParser().extract_contributor_data(sample)
+    assert expected == result
+
+    sample = "Tester Testerson"
+    expected = {
+        "valid_email": False,
+        "first_name": "Tester",
+        "last_name": "Testerson",
+    }
+    result = GithubDataParser().extract_contributor_data(sample)
+    assert expected["valid_email"] is False
+    assert expected["first_name"] == result["first_name"]
+    assert expected["last_name"] == result["last_name"]
+    assert "email" in result
+
+
 """LibraryUpdater Tests"""
 
 
@@ -257,6 +344,23 @@ def test_get_library_list_skip(library_updater):
     gitmodules = [{"module": "litre"}]
     result = library_updater.get_library_list(gitmodules=gitmodules)
     assert result == []
+
+
+def test_update_authors(library_updater, user, library_version):
+    library = library_version.library
+    assert library.authors.exists() is False
+    user.claimed = True
+    user.email = "t_testerson@example.com"
+    user.save()
+
+    library_updater.update_authors(
+        library,
+        ["Tester Testerston <t_testerson -at- example.com>", "Tester2 Testerson2"],
+    )
+    library.refresh_from_db()
+    assert library.authors.exists()
+    assert library.authors.filter(email="t_testerson@example.com").exists()
+    assert library.authors.filter(email="tester2_testerson2@example.com").exists()
 
 
 def test_update_libraries(library_updater):
