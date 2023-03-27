14b950fc1f1dfdfab0abf467177c8c998caf12f5
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-03-27 12:08:19 -0700

    :wrench: Refactor fake email function into util

--- libraries/utils.py
+++ libraries/utils.py
@@ -1,9 +1,21 @@
 import structlog
 
 from dateutil.parser import ParserError, parse
+from django.utils.text import slugify
 
 
 logger = structlog.get_logger()
+
+
+def generate_fake_email(val: str) -> str:
+    """
+    Slugifies a string to make a fake email. Would not necessarily be unique -- this is
+    a lazy way for us to avoid creating multiple new user records for one contributor who
+    contributes to multiple libraries.
+    """
+    slug = slugify(val)
+    local_email = slug.replace("-", "_")[:50]
+    return f"{local_email}@example.com"
 
 
 def parse_date(date_str):
