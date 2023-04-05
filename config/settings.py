ed2e2494d3789b74ab5fb7334770212b51b90640
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-04-05 14:48:58 -0700

    :sparkles: Add caching logic to static content

--- config/settings.py
+++ config/settings.py
@@ -263,7 +263,15 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": f"redis://{REDIS_HOST}:6379",
     },
+    "static_content": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{REDIS_HOST}:6379",
+        "TIMEOUT": env(
+            "STATIC_CACHE_TIMEOUT", default=86400
+        ),  # Cache timeout in seconds: 1 day
+    },
 }
+
 
 HAYSTACK_CONNECTIONS = {
     "default": {
