4471cb47dd75b25bfa64bb25f957d919e563a3cc
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-04-05 08:18:06 -0700

    :wrench: Add a second config option

--- core/tests/test_renderer.py
+++ core/tests/test_renderer.py
@@ -11,11 +11,9 @@ def test_get_s3_keys():
     - "/site/develop/doc/html/about.html" -> "site/develop/doc/html/about.html"
     """
 
-    # assert "/site/develop/tools/auto_index/index.html" in get_s3_keys(
-    #     "marshmallow/index.html"
-    # )
-    # assert "/site/develop/doc/html/about.html" in get_s3_keys("marshmallow/about.html")
-    # assert "/site/develop/rst.css" in get_s3_keys("/rst.css")
+    assert "/site/develop/tools/auto_index/index.html" in get_s3_keys(
+        "marshmallow/index.html"
+    )
     assert "/site/develop/doc/html/about.html" in get_s3_keys("/doc/html/about.html")
     assert "/site/develop/doc/html/about.html" in get_s3_keys("doc/html/about.html")
     assert "/site/develop/doc/html/about.html" in get_s3_keys(
@@ -24,3 +22,6 @@ def test_get_s3_keys():
     assert "/site/develop/doc/html/about.html" in get_s3_keys(
         "site/develop/doc/html/about.html"
     )
+    # assert "/site/develop/doc/html/about.html" in get_s3_keys("marshmallow/about.html")
+    assert "/site/develop/rst.css" in get_s3_keys("/rst.css")
+    
