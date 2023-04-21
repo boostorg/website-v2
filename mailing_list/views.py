0f1908d65efd6caa27653c4361125fd73f50d267
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-04-20 17:42:50 -0700

    :zap: Add mailing list message detail view

--- mailing_list/views.py
+++ mailing_list/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import MailingListMessage
 
 
@@ -9,7 +9,17 @@ class MailingListView(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        # Retrieve only top-level messages (messages without a parent)
         return MailingListMessage.objects.filter(parent__isnull=True).order_by(
             "-sent_at"
         )
+
+
+class MailingListDetailView(DetailView):
+    model = MailingListMessage
+    template_name = "mailing_list/detail.html"
+    context_object_name = "message"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["replies"] = self.object.replies.order_by("sent_at")
+        return context
