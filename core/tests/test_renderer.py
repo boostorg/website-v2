920932328f3338b1eeb23f99deb88a97fbfa8f3e
Frank Wiles <frank@revsys.com>
2023-04-26 09:49:57 -0500

    Black

--- core/tests/test_renderer.py
+++ core/tests/test_renderer.py
@@ -11,7 +11,15 @@ def test_get_s3_keys():
     - "/site/develop/doc/html/about.html" -> "site/develop/doc/html/about.html"
     """
 
-    assert "/site-docs/develop/user-guide/index.html" in get_s3_keys("/doc/user-guide/index.html")
-    assert "/site-docs/develop/contributor-guide/index.html" in get_s3_keys("/doc/contributor-guide/index.html")
-    assert "/site-docs/develop/formal-reviews/index.html" in get_s3_keys("/doc/formal-reviews/index.html")
-    assert "/site-docs/develop/release-process/index.html" in get_s3_keys("/doc/release-process/index.html")
+    assert "/site-docs/develop/user-guide/index.html" in get_s3_keys(
+        "/doc/user-guide/index.html"
+    )
+    assert "/site-docs/develop/contributor-guide/index.html" in get_s3_keys(
+        "/doc/contributor-guide/index.html"
+    )
+    assert "/site-docs/develop/formal-reviews/index.html" in get_s3_keys(
+        "/doc/formal-reviews/index.html"
+    )
+    assert "/site-docs/develop/release-process/index.html" in get_s3_keys(
+        "/doc/release-process/index.html"
+    )
