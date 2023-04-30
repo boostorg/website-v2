b4defd63481feb4b19104a3d38489779d5dccd34
Frank Wiles <frank@revsys.com>
2023-04-30 12:55:47 -0500

    Remove assumption developer has git installed
    We were running `git describe --tags` to get a local
    version for display in the admin.  Catch exceptions thrown when
    git does not exist and use a static string instead.

--- config/settings.py
+++ config/settings.py
@@ -1,16 +1,14 @@
-import environs
 import logging
 import os
-import structlog
 import subprocess
 import sys
-
-from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
+
+import environs
+import structlog
+from django.core.exceptions import ImproperlyConfigured
+from machina import MACHINA_MAIN_STATIC_DIR, MACHINA_MAIN_TEMPLATE_DIR
 from pythonjsonlogger import jsonlogger
-
-from machina import MACHINA_MAIN_TEMPLATE_DIR, MACHINA_MAIN_STATIC_DIR
-
 
 env = environs.Env()
 
@@ -324,10 +322,14 @@ ENV_NAME = env("ENVIRONMENT_NAME", default="Unknown Environment")
 IMAGE_TAG = env("IMAGE_TAG", default="Unknown Version")
 
 if LOCAL_DEVELOPMENT:
-    output = subprocess.check_output(
-        ["git", "describe", "--tags"], universal_newlines=True
-    )
-    IMAGE_TAG = str(output.strip())
+    try:
+        output = subprocess.check_output(
+            ["git", "describe", "--tags"], universal_newlines=True
+        )
+        IMAGE_TAG = str(output.strip())
+    except Exception:
+        print("WARNING: Unable to run git, unable to determine local image tag")
+        IMAGE_TAG = "UNKNOWN-VERSION"
 
 ENVIRONMENT_NAME = f"{ENV_NAME} - {IMAGE_TAG}"
 
