e2d2dd9ed2992cc5be2a522e4237115bc06d8f3a
Natalia <124304+nessita@users.noreply.github.com>
2023-05-30 22:31:46 -0300

    Revert removal of imports

--- versions/tests/test_version_file_api.py
+++ versions/tests/test_version_file_api.py
@@ -1,5 +1,18 @@
+# from django.db import transaction
+# from django.db import IntegrityError
+# from django.core.files import File as DjangoFile
+
+# import tempfile
+# from django.urls import reverse
+# from rest_framework.test import APIClient
+# from test_plus.test import TestCase
+
 # from users.factories import UserFactory, SuperUserFactory, VersionGroupFactory
 # from versions.factories import VersionFactory, VersionFileFactory
+
+# from PIL import Image
+
+# from versions.models import VersionFile
 
 
 # class VersionViewTests(TestCase):
