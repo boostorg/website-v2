f6f1b10a5d77dd761b58d9817cadd5c9368352d8
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-05-31 13:52:04 -0700

    :sparkles: :umbrella: Command to convert based on file input

---
+++ core/tests/test_commands.py
@@ -0,0 +1,37 @@
+import pytest
+from django.core import management
+from django.conf import settings
+import os
+
+
+@pytest.fixture
+def cleanup_test_asciidoc():
+    # This function will be executed after the test function completes
+    def remove_files():
+        expected_output_file = "core/tests/sample.html"
+        os.remove(os.path.join(settings.BASE_DIR, expected_output_file))
+
+    # Yield control back to the test function
+    yield
+    # After the test function completes, execute the cleanup
+    remove_files()
+
+
+@pytest.mark.django_db
+def test_asciidoc_command_creates_output(cleanup_test_asciidoc):
+    input_file = "core/tests/sample.adoc"
+    output_file = "core/tests/sample.html"
+
+    # Create a test AsciiDoc file in your BASE_DIR
+    with open(os.path.join(settings.BASE_DIR, input_file), "w") as f:
+        f.write("== Test Document\n\nThis is a test.")
+
+    # Run the management command
+    management.call_command("convert_ascii", input_file)
+
+    # Check that the output file exists
+    assert os.path.exists(os.path.join(settings.BASE_DIR, output_file))
+
+    # Check that the output contains a div with the id "content"
+    with open(os.path.join(settings.BASE_DIR, output_file), "r") as f:
+        assert 'id="content"' in f.read()
