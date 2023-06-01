e3f37294aafbccbb9331869b588ef12bac7eff8b
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-06-01 15:17:10 -0700

    Add celery task to convert adoc to html

---
+++ core/tests/test_tasks.py
@@ -0,0 +1,55 @@
+import pytest
+import tempfile
+
+from django.core.cache import caches
+from django.test import override_settings
+
+from core.tasks import adoc_to_html
+
+
+@override_settings(
+    CACHES={
+        "default": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "unique-snowflake",
+        },
+        "machina_attachments": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "another-unique-snowflake",
+        },
+        "static_content": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "third-unique-snowflake",
+            "TIMEOUT": "60",  # Cache timeout in seconds: 1 minute
+        },
+    }
+)
+def test_adoc_to_html():
+    # Get the static content cache
+    static_content_cache = caches["static_content"]
+
+    # The content of the sample adoc file
+    sample_adoc_content = "= Document Title\n\nThis is a sample document.\n"
+
+    # Write the content to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(sample_adoc_content.encode())
+        temp_file_path = temp_file.name
+
+    # The cache key to be used
+    cache_key = "sample_key"
+
+    # Execute the task
+    adoc_to_html(temp_file_path, cache_key)
+
+    # Verify that the content has been stored in the cache
+    cached_result = static_content_cache.get(cache_key)
+
+    assert cached_result is not None
+    assert cached_result[0] == "html_content"
+    assert cached_result[1] == "text/html"
+
+    # Verify that the temporary file has been deleted
+    with pytest.raises(FileNotFoundError):
+        with open(temp_file_path, "r"):
+            pass
