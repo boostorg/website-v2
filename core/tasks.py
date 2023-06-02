9494112bf6e17489f10c6d9078f5a9627ab0eb92
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-06-02 10:33:36 -0700

    Return converted html from task

--- core/tasks.py
+++ core/tasks.py
@@ -7,24 +7,26 @@ from celery import shared_task
 
 
 @shared_task
-def adoc_to_html(file_path, cache_key):
-    subprocess.run(
-        ["asciidoctor", "-o", "-", "-"],
-        input=file_path,
-        text=True,
+def adoc_to_html(file_path, cache_key, content_type):
+    result = subprocess.run(
+        ["asciidoctor", "-o", "-", file_path],
         check=True,
         capture_output=True,
     )
 
+    # Get the output from the command
+    converted_html = result.stdout
     # Get the static content cache
     static_content_cache = caches["static_content"]
 
     # Store the HTML content in cache
     static_content_cache.set(
         cache_key,
-        ("html_content", "text/html"),
+        (converted_html, content_type),
         int(settings.CACHES["static_content"]["TIMEOUT"]),
     )
 
     # Delete the temporary file
     os.remove(file_path)
+
+    return converted_html
