8801d0bb5cb6b41e026cfd3ac5833a0956c23ac5
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-06-26 17:51:13 -0700

    Clear redis cache by content type

--- core/managers.py
+++ core/managers.py
@@ -13,3 +13,17 @@ class RenderedContentManager(models.Manager):
         logger.info(
             "rendered_content_manager_delete_by_content_type", content_type=content_type
         )
+
+    def clear_cache_by_content_type(self, content_type):
+        """Clears the static content cache of all rendered content of a given type."""
+        cache = caches["static_content"]
+        results = self.filter(content_type=content_type)
+        for result in results:
+            cache.delete(result.cache_key)
+
+        logger.info(
+            "rendered_content_manager_clear_cache_by_content_type",
+            cache_name="static_content",
+            content_type=content_type,
+            count=len(results),
+        )
