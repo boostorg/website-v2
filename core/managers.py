4dcdeb9c0f94f64f4e5aaea8fb9526969920fb20
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-06-26 17:36:19 -0700

    Add docstring

--- core/managers.py
+++ core/managers.py
@@ -1,5 +1,6 @@
 import structlog
 
+from django.core.cache import caches
 from django.db import models
 
 logger = structlog.get_logger()
@@ -7,6 +8,7 @@ logger = structlog.get_logger()
 
 class RenderedContentManager(models.Manager):
     def delete_by_content_type(self, content_type):
+        """Deletes all rendered content of a given type."""
         self.filter(content_type=content_type).delete()
         logger.info(
             "rendered_content_manager_delete_by_content_type", content_type=content_type
