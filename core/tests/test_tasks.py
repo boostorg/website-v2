a06d9856c0480ceb4f6a936a205e67ef582017b7
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-06-26 17:57:45 -0700

    Add task to clear all rendered content cache

--- core/tests/test_tasks.py
+++ core/tests/test_tasks.py
@@ -2,30 +2,28 @@ import pytest
 import tempfile
 from unittest.mock import patch
 
+from model_bakery import baker
+
 from django.core.cache import caches
 from django.test import override_settings
 
 from core.models import RenderedContent
-from core.tasks import adoc_to_html, clear_rendered_content_by_content_type
-
-
-@override_settings(
-    CACHES={
-        "default": {
-            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-            "LOCATION": "unique-snowflake",
-        },
-        "machina_attachments": {
-            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-            "LOCATION": "another-unique-snowflake",
-        },
-        "static_content": {
-            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-            "LOCATION": "third-unique-snowflake",
-            "TIMEOUT": "60",  # Cache timeout in seconds: 1 minute
-        },
-    }
+from core.tasks import (
+    adoc_to_html,
+    clear_rendered_content_cache_by_content_type,
 )
+
+
+TEST_CACHES = {
+    "static_content": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "third-unique-snowflake",
+        "TIMEOUT": "60",  # Cache timeout in seconds: 1 minute
+    },
+}
+
+
+@override_settings(CACHES=TEST_CACHES)
 def test_adoc_to_html():
     # Get the static content cache
     caches["static_content"]
@@ -49,11 +47,22 @@ def test_adoc_to_html():
             pass
 
 
-def test_clear_rendered_content_by_content_type(rendered_content):
-    assert RenderedContent.objects.filter(
-        content_type=rendered_content.content_type
-    ).exists()
-    clear_rendered_content_by_content_type(rendered_content.content_type)
-    assert not RenderedContent.objects.filter(
-        content_type=rendered_content.content_type
-    ).exists()
+@override_settings(CACHES=TEST_CACHES)
+def test_clear_rendered_content_by_content_type():
+    baker.make("core.RenderedContent", content_type="keep", cache_key="keep")
+    baker.make("core.RenderedContent", content_type="clear", cache_key="clear")
+
+    cache = caches["static_content"]
+    cache.set("keep", "keep")
+    cache.set("clear", "clear")
+
+    assert cache.get("keep") == "keep"
+    assert cache.get("clear") == "clear"
+
+    clear_rendered_content_cache_by_content_type("clear")
+
+    assert cache.get("keep") == "keep"
+    assert cache.get("clear") is None
+
+    assert RenderedContent.objects.filter(content_type="keep").exists()
+    assert not RenderedContent.objects.filter(content_type="clear").exists()
