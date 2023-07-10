b433852afbedde7d323926ca6ad20f83ef90460e
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-07-10 07:46:03 -0700

    Add DetailView for most recent version, update header links

--- versions/views.py
+++ versions/views.py
@@ -3,6 +3,22 @@ from django.views.generic import DetailView
 from versions.models import Version
 
 
+class VersionCurrentReleaseDetail(DetailView):
+    """Web display of list of Versions"""
+
+    model = Version
+    queryset = Version.objects.active()
+    template_name = "releases_temp.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["versions"] = Version.objects.active()
+        return context
+
+    def get_object(self):
+        return Version.objects.most_recent()
+
+
 class VersionDetail(DetailView):
     """Web display of list of Versions"""
 
