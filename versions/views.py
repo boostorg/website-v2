70922af6c9160dd122b108938ef9776b258cf02f
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-07-11 14:07:37 -0700

    Enable form on releases page to get to older releases

--- versions/views.py
+++ versions/views.py
@@ -1,22 +1,44 @@
-from django.views.generic import DetailView
+import structlog
 
+from django.views.generic import DetailView
+from django.views.generic.edit import FormMixin
+from django.shortcuts import redirect
+
+from libraries.forms import VersionSelectionForm
 from versions.models import Version
 
+logger = structlog.get_logger(__name__)
 
-class VersionDetail(DetailView):
+
+class VersionDetail(FormMixin, DetailView):
     """Web display of list of Versions"""
 
+    form_class = VersionSelectionForm
     model = Version
     queryset = Version.objects.active()
     template_name = "versions/detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context["versions"] = Version.objects.active()
+        context["versions"] = Version.objects.active().order_by("-release_date")
         current_version = Version.objects.most_recent()
         obj = self.get_object()
         context["current_release"] = bool(current_version == obj)
+
         return context
+
+    def post(self, request, *args, **kwargs):
+        """User has submitted a form and will be redirected to the right record."""
+        form = self.get_form()
+        if form.is_valid():
+            version = form.cleaned_data["version"]
+            return redirect(
+                "release-detail",
+                slug=version.slug,
+            )
+        else:
+            logger.info("version_detail_invalid_version")
+        return super().get(request)
 
 
 class VersionCurrentReleaseDetail(VersionDetail):
