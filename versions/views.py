473dc49e3d26def0a845c722044b30f45b354920
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-07-11 13:40:50 -0700

    Refactor VersionDetail views to make adding a form easier

--- versions/views.py
+++ versions/views.py
@@ -3,23 +3,6 @@ from django.views.generic import DetailView
 from versions.models import Version
 
 
-class VersionCurrentReleaseDetail(DetailView):
-    """Web display of list of Versions"""
-
-    model = Version
-    queryset = Version.objects.active()
-    template_name = "versions/detail.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        context["versions"] = Version.objects.active()
-        context["current_release"] = True
-        return context
-
-    def get_object(self):
-        return Version.objects.most_recent()
-
-
 class VersionDetail(DetailView):
     """Web display of list of Versions"""
 
@@ -29,7 +12,20 @@ class VersionDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
+        context["versions"] = Version.objects.active()
         current_version = Version.objects.most_recent()
         obj = self.get_object()
         context["current_release"] = bool(current_version == obj)
         return context
+
+
+class VersionCurrentReleaseDetail(VersionDetail):
+    """Web display of list of Versions"""
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["current_release"] = True
+        return context
+
+    def get_object(self):
+        return Version.objects.most_recent()
