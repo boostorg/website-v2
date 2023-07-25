fadf11f0db1c3bcfb8e3945e00797cde15669e96
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-07-25 07:01:38 -0700

    Add a single-page profile update form
    - Add password reset form to single-page profile
    - Add profile photo form to single-page profile
    - Add button to import photo from GitHub
    - Add preferences form to single-page profile
    - Add tests for unauthenticated/authenticated users
    - Add tests that forms are present
    - Add tests that the forms submit, without changing other data

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,6 +1,12 @@
 import pytest
+import tempfile
 
-from ..forms import PreferencesForm
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from allauth.account.forms import ChangePasswordForm
+from PIL import Image
+
+from ..forms import PreferencesForm, UserProfilePhotoForm
 from ..models import Preferences
 
 
@@ -53,6 +59,125 @@ def test_preferences_post_clears_options(
     ):
         expected = original
     assert getattr(user.preferences, form_field) == expected
+
+    assert_messages(
+        response, [("success", "Your preferences were successfully updated.")]
+    )
+
+
+@pytest.mark.django_db
+def test_new_current_user_profile_not_authenticated(tp, user):
+    tp.assertLoginRequired("profile-account-new")
+
+
+@pytest.mark.django_db
+def test_new_current_user_profile_view_get(user, tp):
+    with tp.login(user):
+        response = tp.assertGoodView(tp.reverse("profile-account-new"), verbose=True)
+        assert isinstance(response.context["change_password_form"], ChangePasswordForm)
+        assert isinstance(response.context["profile_photo_form"], UserProfilePhotoForm)
+        assert isinstance(response.context["profile_preferences_form"], PreferencesForm)
+
+
+@pytest.mark.django_db
+def test_new_current_user_profile_view_post_valid_password(user, tp):
+    with tp.login(user):
+        response = tp.post(
+            tp.reverse("profile-account-new"),
+            data={
+                "email": user.email,
+                "oldpassword": "password",
+                "password1": "new_password",
+                "password2": "new_password",
+            },
+            follow=True,
+        )
+        assert response.status_code == 200
+        user.refresh_from_db()
+        user.check_password("new_password")
+
+
+@pytest.mark.django_db
+def test_new_current_user_profile_view_post_invalid_password(user, tp):
+    old_password = "password"
+    with tp.login(user):
+        response = tp.post(
+            tp.reverse("profile-account-new"),
+            data={
+                "email": user.email,
+                "oldpassword": "not the right password",
+                "password1": "new_password",
+                "password2": "new_password",
+            },
+            follow=True,
+        )
+        assert response.status_code == 200
+        user.refresh_from_db()
+        user.check_password(old_password)
+
+
+@pytest.mark.django_db
+def test_new_current_user_profile_view_post_valid_photo(user, tp):
+    """Test that a user can upload a new profile picture."""
+    # Create a temporary image file for testing
+    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_image:
+        image = Image.new("RGB", (200, 200))
+        image.save(temp_image, "jpeg")
+        temp_image.seek(0)
+
+        # Wrap temp_image in Django's File object
+        uploaded_file = SimpleUploadedFile(
+            name="test_image.jpg", content=temp_image.read(), content_type="image/jpeg"
+        )
+
+    with tp.login(user):
+        response = tp.post(
+            tp.reverse("profile-account-new"),
+            data={
+                "image": uploaded_file,
+            },
+            follow=True,
+        )
+        assert response.status_code == 200
+        user.refresh_from_db()
+        assert user.image
+        # confirm that password was not changed, as these are on the same screen
+        assert user.check_password("password")
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("user_type", ["user", "moderator_user"])
+@pytest.mark.parametrize("form_field", PreferencesForm.Meta.fields)
+def test_new_current_user_profile_view_post_valid_preferences(
+    user_type, form_field, tp, request, assert_messages
+):
+    user = request.getfixturevalue(user_type)
+    original_preferences = {
+        field: getattr(user.preferences, field) for field in PreferencesForm.Meta.fields
+    }
+    new_preferences = {
+        field: [] for field in PreferencesForm.Meta.fields
+    }  # Clear all options
+
+    with tp.login(user):
+        response = tp.post(
+            tp.reverse("profile-account-new"),
+            data={**new_preferences, "update_preferences": "Update Preeferences"},
+            follow=True,
+        )
+
+    assert response.status_code == 200
+    user.refresh_from_db()
+    for field in PreferencesForm.Meta.fields:
+        if (
+            field == "allow_notification_others_news_needs_moderation"
+            and user_type != "moderator_user"
+        ):
+            assert (
+                getattr(user.preferences, field) == original_preferences[field]
+            )  # Field should be unchanged for non-moderators
+        else:
+            assert getattr(user.preferences, field) == []  # Field should be cleared
 
     assert_messages(
         response, [("success", "Your preferences were successfully updated.")]
