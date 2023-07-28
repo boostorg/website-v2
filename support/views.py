c300d3d090a547897d8552d6174f51492712d0ea
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-07-28 11:41:05 -0700

    Remove contact form

--- support/views.py
+++ support/views.py
@@ -1,32 +1,9 @@
 from django.views.generic import TemplateView
-from .forms import ContactForm
 
 
-class SupportBaseView(TemplateView):
+class SupportView(TemplateView):
     """
-    Generic base view for the contact form
-    used on contact and support views
-    """
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["form"] = ContactForm
-        return context
-
-
-class SupportView(SupportBaseView):
-    """
-    View for the Support page with contact form
-    TODO: Add reCaptcha
+    Generic base view for the support view
     """
 
     template_name = "support/support.html"
-
-
-class ContactView(SupportBaseView):
-    """
-    View for the Support page with contact form
-    TODO: Add reCaptcha
-    """
-
-    template_name = "support/contact.html"
