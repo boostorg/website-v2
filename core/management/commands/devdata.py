fd14322d936e1513923e4a4710fb4999f7136a5e
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-09-07 14:25:26 -0700

    Add devdata command that calls import versions with a version count and an optional token

--- core/management/commands/devdata.py
+++ core/management/commands/devdata.py
@@ -4,7 +4,9 @@ from django.core.management import call_command
 
 
 @click.command()
+@click.option(
+    "--max-version-count", type=int, default=100, help="Number of versions to import."
+)
 @click.option("--token", is_flag=False, help="Github API token")
-def command(token):
-    call_command("import_versions", token=token)
-    pass
+def command(max_version_count, token):
+    call_command("import_versions", token=token, max_count=max_version_count)
