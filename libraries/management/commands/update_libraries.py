4712551827ede888d34f107e8ec3399952c2370c
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-09-07 15:08:46 -0700

    Improvements to import/update libraries command
    - Remove outdated options, quiet some logging
    - Don't add authors yet -- need to add maintainers first
    - Add GitHub token option to update_libraries

--- libraries/management/commands/update_libraries.py
+++ libraries/management/commands/update_libraries.py
@@ -5,32 +5,12 @@ from libraries.github import LibraryUpdater
 
 @click.command()
 @click.option("--local", is_flag=True, default=False)
-@click.option("--update_monthly_commit_counts", is_flag=True, default=True)
-@click.option("--update_first_tag_date", is_flag=True, default=True)
-def command(local, update_monthly_commit_counts, update_first_tag_date):
+@click.option("--token", is_flag=False, help="Github API token")
+def command(local, token):
     """
     Calls the LibraryUpdater, which retrieves the active boost libraries
     from the Boost repo and updates the models in our database with the latest
-    information on that library (repo) and its issues, pull requests, and related
-    objects from GitHub.
-
-    If the --local flag is set, then the monthly commit counts and first tag date
-    will not be updated. This is useful for testing, since the monthly commit counts
-    and first tag date are not updated very often, and it takes a long time to
-    retrieve them from GitHub.
-
-    If the --update_monthly_commit_counts flag is set, then the monthly commit counts
-    will be updated. This can take a long time.
-
-    If the --update_first_tag_date flag is set, then the first tag date will be updated.
-    This can take a long time.
+    information on that library (repo) from GitHub.
     """
-    updater = LibraryUpdater()
-
-    if local:
-        update_monthly_commit_counts = False
-        update_first_tag_date = False
-    updater.update_libraries(
-        update_monthly_commit_counts=update_monthly_commit_counts,
-        update_first_tag_date=update_first_tag_date,
-    )
+    updater = LibraryUpdater(token=token)
+    updater.update_libraries()
