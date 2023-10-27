ed2351a4e4e88cf63fff405c208197b5ce76c77c
Greg Newman <greg@gregnewman.org>
2023-10-27 09:02:39 -0400

    :bug: Fixes dark mode for charts and search
    This problem only seemed to be an issue in Firefox so adding an explicity setting if matchMedia finds dark.
    
    Issue: #729

--- static/js/DetectMode.js
+++ static/js/DetectMode.js
@@ -11,5 +11,6 @@ if (window.localStorage) {
 }
 
 if (explicitelyPreferScheme !== 'light' && window.matchMedia('(prefers-color-scheme:dark)').matches) {
+    localStorage.colorMode = 'dark';  // explicitly set `colorMode` to system if there is no preference. this fixes charts and search default mode in Firefox.
     document.documentElement.classList.add('dark');
 }
