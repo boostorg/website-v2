909241c990329ac0548a842e57be879fdc1492a6
Lacey Williams Henschel <laceynwilliams@gmail.com>
2023-12-11 09:07:52 -0800

    Hide development versions from the dropdown list on the releases page
    - Add method to return a different version dropdown
    - Use stricter version drop-down on versions page

--- versions/tests/test_managers.py
+++ versions/tests/test_managers.py
@@ -80,6 +80,56 @@ def test_version_dropdown(
     assert list(queryset) == expected
 
 
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "version_name, beta, full_release, most_recent_name, most_recent_beta_name, should_be_included",  # noqa
+    [
+        # Version is the most recent, full release
+        ("1.84.0", False, True, "1.84.0", "1.85.0.beta1", True),
+        # Version is the most recent beta, not full release
+        ("1.85.0.beta1", True, False, "1.84.0", "1.85.0.beta1", True),
+        # Version is not the most recent, not full release
+        ("1.83.0", False, False, "1.84.0", "1.85.0.beta1", False),
+        # No beta, version is full release
+        ("1.84.0", False, True, "1.84.0", None, True),
+        # No beta, version is not full release
+        ("develop", False, False, "1.84.0", None, False),
+    ],
+)
+def test_version_dropdown_strict(
+    version_name,
+    beta,
+    full_release,
+    most_recent_name,
+    most_recent_beta_name,
+    should_be_included,
+    version,
+    beta_version,
+):
+    """Test the version_dropdown_strict method"""
+
+    # Additional setup for most recent non-beta version
+    most_recent_version = Version.objects.create(
+        name=most_recent_name, beta=False, full_release=True
+    )
+    most_recent_version.save()
+
+    # Setup version instance
+    version.name = version_name
+    version.beta = beta
+    version.full_release = full_release
+    version.save()
+
+    if most_recent_beta_name:
+        beta_version.name = most_recent_beta_name
+        beta_version.beta = True
+        beta_version.full_release = False
+        beta_version.save()
+
+    queryset = Version.objects.version_dropdown_strict()
+    assert (version in queryset) == should_be_included
+
+
 def test_active_file_manager(version, inactive_version):
     assert Version.objects.active().count() == 1
     assert VersionFile.objects.active().count() == 1
