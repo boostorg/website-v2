b2942d13dc2cc4e80a0f2b03000a5475fa51bfc3
Spencer Strickland <49487079+4down@users.noreply.github.com>
2024-01-26 14:15:20 -0600

    Truncate latest + homepage and news list

--- core/templatetags/text_helpers.py
+++ core/templatetags/text_helpers.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+import re
 
 register = template.Library()
 
@@ -15,6 +16,30 @@ def truncate_middle(value, arg):
         return value
     else:
         return f"{value[:ln//2]}....{value[-((ln+1)//2):]}"
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def multi_truncate_middle(value, arg):
+    def replace_match(match):
+        word = match.group(0)
+        if len(word) > ln:
+            start = word[: ln // 2]
+            end = word[-((ln + 1) // 2) :]
+            return f"{start}...{end}"
+        return word
+
+    pattern = re.compile(r"\b\w+\b")
+
+    try:
+        ln = int(arg)
+    except ValueError:
+        return value
+    if len(value) <= ln:
+        return value
+    else:
+        result = pattern.sub(replace_match, value)
+        return result
 
 
 @register.filter(is_safe=True)
