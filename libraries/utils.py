2ed461d294ccf8e979a3c5ff441c54012925fd49
Lacey Williams Henschel <laceynwilliams@gmail.com>
2024-02-09 11:12:04 -0800

    Add exception logic for some versions of Member Function docs; fix tests

--- libraries/utils.py
+++ libraries/utils.py
@@ -61,6 +61,22 @@ def generate_library_docs_url_v4(boost_url_slug, library_slug):
     return f"/doc/libs/{boost_url_slug}/doc/html/{library_slug}.html"
 
 
+def generate_library_docs_url_bind_v1(boost_url_slug, library_slug):
+    """Generate a documentation url with a specific format
+
+    First used for Member Function, versions 1.60.0 and older
+    """
+    return f"/doc/libs/{boost_url_slug}/libs/bind/doc/html/{library_slug}.html"
+
+
+def generate_library_docs_url_bind_v2(boost_url_slug, library_slug):
+    """Generate a documentation url with a specific format
+
+    First used for Member Function, versions 1.60.0 and older
+    """
+    return f"/doc/libs/{boost_url_slug}/libs/bind/{library_slug}.html"
+
+
 def generate_library_docs_url_math_v1(boost_url_slug, library_slug):
     """Generate a documentation url with a specific format
 
