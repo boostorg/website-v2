4860811f9bd048185ad3fd5fff75c2c2920c84a3
Kenneth Reitz <me@kennethreitz.org>
2024-04-02 16:33:19 -0400

    Add import_archives_release_data command

--- versions/management/commands/import_archives_release_data.py
+++ versions/management/commands/import_archives_release_data.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 from versions.models import Version
 from versions.releases import (
-    get_artifactory_download_data,
-    get_artifactory_download_uris_for_release,
+    get_archives_download_data,
+    get_archives_download_uris_for_release,
     store_release_downloads_for_version,
 )
 
@@ -22,7 +22,7 @@ def command(release):
     specified release. If no release is specified, it will import the release data
     for all releases included in Artifactory.
     """
-    last_release = settings.MIN_ARTIFACTORY_RELEASE
+    last_release = settings.MIN_ARCHIVES_RELEASE
 
     if release:
         versions = Version.objects.filter(name__icontains=release)
@@ -32,17 +32,17 @@ def command(release):
     for v in versions:
         version_num = v.name.replace("boost-", "")
         try:
-            artifactory_data = get_artifactory_download_uris_for_release(version_num)
+            archives_data = get_archives_download_uris_for_release(version_num)
         except requests.exceptions.HTTPError:
             print(f"Skipping {version_num}, error retrieving release data")
             continue
 
         download_data = []
-        for d in artifactory_data:
+        for d in archives_data:
             try:
-                data = get_artifactory_download_data(d)
+                data = get_archives_download_data(d)
                 download_data.append(data)
-            except requests.exceptions.HTTPError:
+            except (requests.exceptions.HTTPError, ValueError):
                 print(f"Skipping {d}, error retrieving download data")
                 continue
 
