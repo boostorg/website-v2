d6d97a951a229c8e0e784f517d4a53a70efcf831
Kenneth Reitz <me@kennethreitz.org>
2024-04-03 09:33:33 -0400

    Fix conditional statement in settings.py

--- config/settings.py
+++ config/settings.py
@@ -313,7 +313,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 SOCIALACCOUNT_QUERY_EMAIL = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-if not LOCAL_DEVELOPMENT:
+if LOCAL_DEVELOPMENT:
     # This is the default value for the development environment.
     # This enables the tests to run.
     SITE_ID = 1
