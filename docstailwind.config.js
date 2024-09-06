ec6fd4de04d77969443c1c396e6f86d9c0af0e8e
rbbeeston <rob@cppalliance.org>
2024-09-06 12:33:56 -0700

    Font update, home page, boostbook V2

--- docstailwind.config.js
+++ docstailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        'sans': ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', '"Helvetica Neue"', 'sans-serif'],
+        'sans': ['"Noto Sans"', 'sans-serif'],
       }
     }
   },
