b1855a29be78b7de514d3578306b8f5585e1d197
Greg Kaleka <greg@gregkaleka.com>
2024-10-09 16:11:32 -0400

    Display a default message when library is missing a README

--- libraries/constants.py
+++ libraries/constants.py
@@ -309,6 +309,9 @@ SKIP_LIBRARY_VERSIONS = {
 # List of versions for which we know docs are missing
 VERSION_DOCS_MISSING = ["boost-1.33.0"]
 
+# This constant is for library-versions missing a README
+README_MISSING = "⚠️ This library has no README.md or library-details.adoc; consider contributing one."
+
 DEFAULT_LIBRARIES_LANDING_VIEW = "libraries-grid"
 SELECTED_BOOST_VERSION_COOKIE_NAME = "boost_version"
 SELECTED_LIBRARY_VIEW_COOKIE_NAME = "library_view"
