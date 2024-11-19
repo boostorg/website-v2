8094e5b92d4bee7313575d080339f9a01116c647
Your Name <bperrett@fusionbox.com>
2024-11-19 12:51:17 -0800

    Add staff-editable wordcloud ignore list in the admin
    - Fixes #1464
    - Add a SiteSettings singleton model which will hold dynamic site
      settings.

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import RenderedContent
+from .models import RenderedContent, SiteSettings
 
 
 @admin.register(RenderedContent)
 class RenderedContentAdmin(admin.ModelAdmin):
     list_display = ("cache_key", "content_type")
     search_fields = ("cache_key",)
+
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ("id", "wordcloud_ignore")
+
+    def has_add_permission(self, request):
+        return super().has_add_permission(request) and SiteSettings.objects.count() == 0
+
+    def has_delete_permission(self, request, obj=None):
+        return False
