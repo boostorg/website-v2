ff44a9aea5210334d6f1a8840a4e091ef307a3ce
Dave O'Connor <docgithub@dead-pixels.org>
2024-11-22 16:58:23 -0800

    Added styling for disabled state on select dropdowns (#1316)

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,19 @@ module.exports = {
       silver: '#B5C9D3',
     },
     extend: {
+      colors: {
+        slate: {
+          100: '#C2D8E2',
+          200: '#A1C2D0',
+          300: '#7FACBE',
+          400: '#5F96AC',
+          500: '#314A57',
+          600: '#2A404F',
+          700: '#233847',
+          800: '#1C2C3F',
+          900: '#151E37',
+        }
+      },
       fontFamily: {
         'sans': [
           '"Noto Sans"',
