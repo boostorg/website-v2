0384ee994e341b20c1a2bb3c0554d8693b37cdbf
Greg Kaleka <greg@gregkaleka.com>
2024-11-23 09:56:46 -0500

    Fix straggler links to old review process page (#1487)

--- core/tests/test_renderer.py
+++ core/tests/test_renderer.py
@@ -152,9 +152,6 @@ def test_get_s3_keys():
     assert "/site-docs/develop/contributor-guide/index.html" in get_s3_keys(
         "/doc/contributor-guide/index.html"
     )
-    assert "/site-docs/develop/formal-reviews/index.html" in get_s3_keys(
-        "/doc/formal-reviews/index.html"
-    )
     assert "/site-docs/develop/release-process/index.html" in get_s3_keys(
         "/doc/release-process/index.html"
     )
