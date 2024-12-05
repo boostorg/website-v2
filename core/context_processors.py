0bd5de8f3b4ae272a14b9a6f33dde2c48114dd08
Greg Kaleka <greg@gregkaleka.com>
2024-12-05 17:45:09 -0500

    Move active nav item logic to context processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -8,6 +8,25 @@ def current_version(request):
     return {"current_version": Version.objects.most_recent()}
 
 
+def active_nav_item(request):
+    """Custom context processor that adds the active nav item to the context"""
+    path = request.path
+    default_nav_item = "home"
+    path_map = {
+        "/news/": "news",
+        "/doc/": "learn",
+        "/docs/": "learn",
+        "/community/": "community",
+        "/library/": "libraries",
+        "/libraries/": "libraries",
+        "/releases/": "releases",
+    }
+    for prefix, item in path_map.items():
+        if path.startswith(prefix):
+            return {"active_nav_item": item}
+    return {"active_nav_item": default_nav_item}
+
+
 def debug(request):
     """
     Adds settings.DEBUG to the context.
