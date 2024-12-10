ede21f0b29bfd0a4f79eaba2558dbf33b9698144
Greg Kaleka <greg@gregkaleka.com>
2024-12-10 17:39:54 -0500

    noqa tweak

--- core/boostrenderer.py
+++ core/boostrenderer.py
@@ -288,5 +288,5 @@ class BoostRenderer(PygmentsRenderer):
         super().__init__(Youtube)
 
     def render_youtube(self, token):
-        template = '<iframe width="560" height="315" src="https://www.youtube.com/embed/{target}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'  # noqa
+        template = '<iframe width="560" height="315" src="https://www.youtube.com/embed/{target}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'  # noqa E501
         return template.format(target=token.target)
