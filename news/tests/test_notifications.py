7468f54199472a5f324e7de998f453fd08c56030
Greg Kaleka <greg@gregkaleka.com>
2025-01-15 14:54:28 -0500

    News moderation email magic links (#1600)

--- news/tests/test_notifications.py
+++ news/tests/test_notifications.py
@@ -1,15 +1,19 @@
 from datetime import date
 
 import pytest
+from django.conf import settings
 from django.core import mail
 from django.utils.html import escape
 from django.urls import reverse
+from itsdangerous import URLSafeTimedSerializer
 
+from ..constants import NEWS_APPROVAL_SALT
 from ..models import NEWS_MODELS
 from ..notifications import (
     send_email_news_approved,
     send_email_news_needs_moderation,
     send_email_news_posted,
+    generate_magic_approval_link,
 )
 from users.models import Preferences
 
@@ -97,8 +101,8 @@ def test_send_email_news_needs_moderation(
     with tp.assertNumQueriesLessThan(2, verbose=True):
         result = send_email_news_needs_moderation(request, entry)
 
-    assert result == 1
-    assert len(mail.outbox) == 1
+    assert result == 4
+    assert len(mail.outbox) == 4
     msg = mail.outbox[0]
     assert "news entry needs moderation" in msg.subject.lower()
     assert entry.title in msg.body
@@ -107,9 +111,36 @@ def test_send_email_news_needs_moderation(
     assert entry.author.email in msg.body
     assert request.build_absolute_uri(entry.get_absolute_url()) in msg.body
     assert request.build_absolute_uri(reverse("news-moderate")) in msg.body
-    assert msg.recipients() == sorted(
-        [other_moderator.email, moderator_user.email, superuser.email, forth.email]
+    recipients = []
+    for msg in mail.outbox:
+        recipients.extend(msg.recipients())
+    assert set(recipients) == {
+        other_moderator.email,
+        moderator_user.email,
+        superuser.email,
+        forth.email,
+    }
+
+
+def test_generate_magic_approval_link(make_entry, make_user):
+    entry = make_entry()
+    moderator = make_user(groups={"moderator": ["news.*"]}, email="mod@x.com")
+    url = generate_magic_approval_link(entry.slug, moderator.id)
+
+    dummy_token = "dummy-token"
+    expected_base_url = (
+        reverse("news-magic-approve", kwargs={"token": dummy_token})
+        .replace(dummy_token, "")
+        .rstrip("/")
     )
+    assert url.startswith(expected_base_url)
+
+    token = url.split(expected_base_url)[-1].strip("/")
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    data = serializer.loads(token, salt=NEWS_APPROVAL_SALT)
+
+    assert data["entry_slug"] == entry.slug
+    assert data["moderator_id"] == moderator.id
 
 
 @pytest.mark.parametrize("model_class", NEWS_MODELS)
