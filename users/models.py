ab2ac99b947df420526da60a11a59d5a13efe712
Foo Bar <docgithub@dead-pixels.org>
2025-03-03 18:46:47 +0000

    Commit Author and User Model name fall back (#1629) (#1643)

--- users/models.py
+++ users/models.py
@@ -199,7 +199,11 @@ class User(BaseUser):
     """
 
     badges = models.ManyToManyField(Badge)
+    # todo: consider making this unique=True after checking user data for duplicates
     github_username = models.CharField(_("github username"), max_length=100, blank=True)
+    is_commit_author_name_overridden = models.BooleanField(
+        default=False, help_text="Select to override the commit author with Username"
+    )
     image = models.FileField(
         upload_to="profile-images",
         null=True,
@@ -290,6 +294,13 @@ class User(BaseUser):
         if not self.github_username:
             return None
         return f"https://github.com/{self.github_username}"
+
+    @staticmethod
+    def get_user_by_github_url(url: str):
+        if not url:
+            return None
+        github_user = url.rstrip("/").split("/")[-1]
+        return User.objects.filter(github_username=github_user).first()
 
     @transaction.atomic
     def delete_account(self):
