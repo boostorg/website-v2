aa8325f7fd64b316cc9fa9fce2440ceccd8de5dd
Sam Darwin <samuel.d.darwin@gmail.com>
2025-08-20 15:44:32 -0600

    Migrate Plausible data-domain from preview.boost.org to boost.org (#1890)

--- reports/constants.py
+++ reports/constants.py
@@ -1,5 +1,5 @@
 WORDCLOUD_FONT = "NotoSansMono-Regular.ttf"
-WEB_ANALYTICS_DOMAIN = "preview.boost.org"
+WEB_ANALYTICS_DOMAIN = "boost.org"
 WEB_ANALYTICS_API_URL = (
     f"https://plausible.io/api/stats/{WEB_ANALYTICS_DOMAIN}/top-stats/?period=custom"
     "&from={:%Y-%m-%d}&to={:%Y-%m-%d}"
