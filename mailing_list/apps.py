0d0ce09c31bc907c9fb4a9b5b4eaf1dbaecaefb8
Greg Kaleka <greg@gregkaleka.com>
2026-01-05 10:12:55 -0500

    Add verbose name to mailing_list app

--- mailing_list/apps.py
+++ mailing_list/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MailingListConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing_list"
+    verbose_name = "Mailing List"
