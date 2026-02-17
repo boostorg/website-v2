fb8dd13007579929942d4c6af50a528d94ffd17d
Jeremy Childers <jeremy@cppalliance.org>
2026-02-17 14:46:38 -0500

    PR Refactor method of storing and retrieving files to match old system

--- patches/views.py
+++ patches/views.py
@@ -1,32 +1,41 @@
+from pathlib import Path
+
+from django.conf import settings
 from django.http import FileResponse
+from django.http import HttpResponseNotFound
 from django.views.generic import View
 from django.views.generic import TemplateView
-from patches.models import LibraryPatch
-from versions.models import Version
+
+base_dir = settings.BASE_DIR
+PATCHES_PATH = Path(base_dir / "patches" / "files")
 
 
 # Create your views here.
 class PatchListView(TemplateView):
-    model = LibraryPatch
     template_name = "patches/patch_list_view.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         object_dict = {}
-        model = self.model
-        for v in Version.objects.all().order_by("-name"):
-            if model_qs := model.objects.filter(version=v).order_by("patch_name"):
-                object_dict[v.display_name] = model_qs
+        version_folders = [x for x in PATCHES_PATH.iterdir() if x.is_dir()]
+        version_folders.sort(reverse=True)
+        for v in version_folders:
+            version_name = (".").join(v.name.split("_"))
+            object_dict[version_name] = sorted(
+                [x.name for x in v.iterdir() if x.is_file()]
+            )
 
         context["grouped_patches"] = object_dict
         return context
 
 
 class PatchDetailView(View):
-    model = LibraryPatch
-
     def get(self, request, *args, **kwargs):
-        model = self.model
         patch_name = kwargs.get("patch_name")
-        patch = model.objects.get(patch_name=patch_name)
-        return FileResponse(patch.patch_file.open("rb"))
+        patch_files = [x for x in PATCHES_PATH.glob(f"*/{patch_name}")]
+        print(patch_name)
+        if len(patch_files) < 1:
+            return HttpResponseNotFound()
+        patch = patch_files[0]
+
+        return FileResponse(patch.open("rb"))
