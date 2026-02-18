26ad1c231e9b08bea0aea62a28d1800584f395fd
Gabriel Garcia <ygabo@pm.me>
2026-02-18 14:14:38 -0500

    feat: v3 css foundations and v3 as a context processor

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "core.context_processors.current_version",
                 "core.context_processors.active_nav_item",
+                "core.context_processors.v3_enabled",
                 "core.context_processors.debug",
             ],
             "loaders": [
