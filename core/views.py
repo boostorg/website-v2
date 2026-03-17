143c95083b6653369b6fe24483a04982a10cca20
DrJfrost <julianrojas99@hotmail.com>
2026-03-17 12:56:00 -0500

    Adding backgrounds with dynamic images in Heros

--- core/views.py
+++ core/views.py
@@ -1144,12 +1144,23 @@ class V3ComponentDemoView(TemplateView):
         context["create_account_card_preview_url"] = (
             f"{settings.STATIC_URL}img/checker.png"
         )
-        context["hero_image_url"] = f"{settings.STATIC_URL}img/v3/home-page/heros.png"
-        context["hero_image_url_light"] = (
+        context["hero_background_image_url"] = (
+            f"{settings.STATIC_URL}img/v3/home-page/home-page-background.png"
+        )
+        context["hero_legacy_image_url_light"] = (
             f"{settings.STATIC_URL}img/v3/home-page/heros.png"
         )
-        context["hero_image_url_dark"] = (
+        context["hero_legacy_image_url_dark"] = (
             f"{settings.STATIC_URL}img/v3/home-page/heros_light.png"
+        )
+        context["hero_image_url"] = (
+            f"{settings.STATIC_URL}img/v3/home-page/home-page-foreground.png"
+        )
+        context["hero_image_url_light"] = (
+            f"{settings.STATIC_URL}img/v3/home-page/home-page-foreground.png"
+        )
+        context["hero_image_url_dark"] = (
+            f"{settings.STATIC_URL}img/v3/home-page/home-page-foreground.png"
         )
         context["basic_card_data"] = {
             "title": "Found a Bug?",
