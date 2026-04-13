d14ff8bcf0ce97a04b3f078e85a7db9fc96c293e
Teodoro B. Mendes <teodoro.mendes@metalab.com>
2026-04-13 10:52:25 -0300

    feat: add docs and improve aws profile name

--- scripts/sync-large-static-images.sh
+++ scripts/sync-large-static-images.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 DEFAULT_BUCKET="stage.boost.org.v2"
 S3_BUCKETS="boost.org.v2 boost.org-cppal-dev-v2 ${DEFAULT_BUCKET}"
-AWS_PROFILE='upload-images'
+AWS_PROFILE='sync-boost-images'
 DEFAULT_DEST="/static/img/v3"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
