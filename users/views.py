1b93e2d192d3b44d345e04dc45d209abba8c0672
Jeremy Childers <jeremy@cppalliance.org>
2026-04-30 14:31:39 -0400

    Complete unfilled version

--- users/views.py
+++ users/views.py
@@ -98,7 +98,7 @@ class CurrentUserProfileView(
     v3_template_name = "v3/user_profile_page.html"
 
     def get_v3_context_data(self, **kwargs):
-        return {
+        ctx = {
             "github_activity_card_data": {
                 "title": "Latest Boost Github activity",
                 "markdown_text": dedent(
@@ -113,21 +113,53 @@ class CurrentUserProfileView(
                 "button_url": "https://www.github.com",
                 "button_label": "View on Github",
             },
-            "mail_list_card_data": {
+            "mailing_list_activity_card_data": {
                 "title": "Mailing List Activity",
-                "markdown_text": dedent(
-                    """
-                    * Created 24 Commits in [7 repositories](https://www.example.com)
-                    * Created [1 repository](https://www.example.com)
-                    * Created a pull request in cppalliance/buffers that received [6 comments](https://www.example.com)
-                    * Opened 17 other pull requests in [6 repositories](https://www.example.com)
-                    * Reviewed 3 pull requests in [3 repositories](https://www.example.com)
-                """
-                ),
-                "button_url": "https://www.example.com.com",
-                "button_label": "View all activity",
+                "mailing_list_items": [
+                    {
+                        "date": datetime.date(2025, 7, 11),
+                        "headline": "[release] Boost 1.90.0 Beta 1 Release Candidate 1 is available",
+                        "url": "#",
+                    },
+                    {
+                        "date": datetime.date(2025, 7, 11),
+                        "headline": "[release] Boost 1.90.0 Beta 1 Release Candidate 1 is available",
+                        "url": "#",
+                    },
+                    {
+                        "date": datetime.date(2025, 7, 11),
+                        "headline": "[release] Boost 1.90.0 Beta 1 Release Candidate 1 is available",
+                        "url": "#",
+                    },
+                    {
+                        "date": datetime.date(2025, 7, 11),
+                        "headline": "[release] Boost 1.90.0 Beta 1 Release Candidate 1 is available",
+                        "url": "#",
+                    },
+                    {
+                        "date": datetime.date(2025, 7, 11),
+                        "headline": "[release] Boost 1.90.0 Beta 1 Release Candidate 1 is available",
+                        "url": "#",
+                    },
+                ],
             },
+            "posts": [
+                {
+                    "title": "Share Your Knowledge with the community",
+                    "summary": "Write posts to share ideas, tutorials, announcements, or lessons learned about working with Boost",
+                }
+            ],
         }
+        badge_img = f"{settings.STATIC_URL}img/v3/badges"
+        ctx["badge_icon_srcs"] = [
+            f"{badge_img}/badge-first-place.png",
+            f"{badge_img}/badge-second-place.png",
+            f"{badge_img}/badge-bronze.png",
+            f"{badge_img}/badge-gold-medal.png",
+            f"{badge_img}/badge-military-star.png",
+        ]
+
+        return ctx
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
