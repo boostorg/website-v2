1baf0f0da1991d1a892314d1a1d06839b82858d8
sdarwin <samuel.d.darwin@gmail.com>
2026-05-06 19:54:06 +0200

    Mailman api variables

--- config/settings.py
+++ config/settings.py
@@ -379,6 +379,11 @@ else:
 
 MAILMAN_CORE_DATABASE = env("MAILMAN_CORE_DATABASE", default="unknown")
 
+# Mailman API credentials
+MAILMAN_REST_API_URL = env("MAILMAN_REST_API_URL", default="http://localhost:8001")
+MAILMAN_REST_API_USER = env("MAILMAN_REST_API_USER", default="restadmin")
+MAILMAN_REST_API_PASS = env("MAILMAN_REST_API_PASS", default="restpass")
+
 # Fastly API credentials
 FASTLY_SERVICE = env("FASTLY_SERVICE", default="empty")
 FASTLY_SERVICE2 = env("FASTLY_SERVICE2", default="empty")
