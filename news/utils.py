ba73eb1f534bbfecc68972a9906b7ad600ee9ae2
Jeremy Childers <jeremy@cppalliance.org>
2026-05-15 10:40:15 -0400

    PR: Feedback: Use thumbnail functionality and thorough test cases

--- news/utils.py
+++ news/utils.py
@@ -1,4 +1,3 @@
-import math
 import os
 import requests
 import typing
@@ -72,28 +71,18 @@ def downsize_uploaded_image(image: UploadedFile) -> UploadedFile:
             file_name = root
             file_name += ".webp"
 
-        width, height = im.size
-        p_width, p_height = None, None  # Preferred output image width and height
         s_width, s_height = (
             settings.DOWNSCALED_IMAGE_WIDTH,
             settings.DOWNSCALED_IMAGE_HEIGHT,
         )  # Settings based preferred width and height
 
-        # Scale the preferred width and height in a proportional manner, to not skew the image
-
-        # Scale based on the dimension that is further off from preferred dimensions
-        if width - s_width >= height - s_height:
-            p_width = s_width
-            p_height = math.floor((p_width / width) * height)
-        else:
-            p_height = s_height
-            p_width = math.floor((p_height / height) * width)
-
-        r_image = im.resize((p_width, p_height))
+        # Use the thumbnail functionality to reduce this image to our preferred width and height.
+        # Maintains aspect ratio, and won't ever upscale.
+        im.thumbnail((s_width, s_height))
 
         # Save to a BytesIO, actual file system saving will be handled by the form calling this function
         img = BytesIO()
-        r_image.save(img, format="webp")
+        im.save(img, format="webp")
         img.seek(0)
         return UploadedFile(
             img,
