32e7838095a52f9d7175b5d7b25ae6cae6ec15b8
Jeremy Childers <jeremy@cppalliance.org>
2026-05-19 14:35:40 -0400

    Init

--- reports/constants.py
+++ reports/constants.py
@@ -4,3 +4,11 @@ WEB_ANALYTICS_API_URL = (
     f"https://plausible.io/api/stats/{WEB_ANALYTICS_DOMAIN}/top-stats/?period=custom"
     "&from={:%Y-%m-%d}&to={:%Y-%m-%d}"
 )
+WEB_ANALYTICS_API_URL_V2 = "https://plausible.io/api/v2/query"
+WEB_ANALYSTICS_API_TOP_STATS_PAYLOAD = {
+    "site_id": "dummy.site",
+    "metrics": ["visitors", "pageviews", "bounce_rate"],
+    "date_range": "7d",
+    "filters": [["is_not", "visit:country_name", [""]]],
+    "dimensions": ["visit:country_name", "visit:city_name"],
+}
