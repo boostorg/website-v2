d528dcf137861bacddbf1f835cc24471d795240e
Teodoro B. Mendes <teodoro.mendes@metalab.com>
2026-06-02 12:50:35 -0300

    refactor: convert Mailman client to a class

--- mailing_list/views.py
+++ mailing_list/views.py
@@ -18,14 +18,13 @@ from django.utils.timesince import timesince
 from django.views import View
 
 from mailing_list.client import MailmanAPIError
-from mailing_list.client import is_confirmed as mailman_is_confirmed
-from mailing_list.client import subscribe as mailman_subscribe
-from mailing_list.client import unsubscribe as mailman_unsubscribe
+from mailing_list.client import MailmanClient
 from mailing_list.constants import MAILING_LIST_LABELS
 from mailing_list.models import SubscriptionStatus
 from mailing_list.models import UserMailingListSubscription
 
 logger = logging.getLogger(__name__)
+_mailman = MailmanClient()
 
 _CONFIRM_SALT = "mailing-list-confirm-a40b24dc-a26d-49ca-81d1-5b2fccb5fd7b"
 _CONFIRM_MAX_AGE = 7 * 24 * 60 * 60  # 7 days in seconds
@@ -183,7 +182,7 @@ class SubscribeView(LoginRequiredMixin, View):
                 unsubscribed.append(list_id)
                 continue
             try:
-                mailman_unsubscribe(email, list_id)
+                _mailman.unsubscribe(email, list_id)
                 UserMailingListSubscription.objects.filter(
                     user=request.user, list_id=list_id
                 ).delete()
@@ -373,7 +372,7 @@ class QuickSubscribeView(View):
 
     def _handle_anonymous(self, request, email, list_id):
         try:
-            if mailman_is_confirmed(email, list_id):
+            if _mailman.is_confirmed(email, list_id):
                 if _is_htmx(request):
                     return self._card(
                         request,
@@ -457,7 +456,7 @@ class ConfirmSubscriptionView(View):
 
         for list_id in list_ids:
             try:
-                mailman_subscribe(email, list_id)
+                _mailman.subscribe(email, list_id)
                 if user is not None:
                     UserMailingListSubscription.objects.filter(
                         user=user, list_id=list_id
