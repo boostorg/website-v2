4d9786f11c6df34397d5fd43c82c9de7f24c3d9d
Cristian Yáñez <cristian.yanez.canales@gmail.com>
2026-07-28 09:54:58 -0400

    fix: create merge migration for 0022_user_country_user_hide_badges_and_more

--- users/migrations/0022_user_country_user_hide_badges_and_more.py
+++ users/migrations/0022_user_country_user_hide_badges_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("users", "0023_user_biography_user_tagline"),
+        ("users", "0021_add_v3_testers_group"),
     ]
 
     operations = [
