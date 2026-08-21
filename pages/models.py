37089aa1961603f045527f5c4fb994288e2e7cf6
Jeremy Childers <jeremy@cppalliance.org>
2026-08-21 17:54:09 -0400

    Check V3 flag in routing logic

--- pages/models.py
+++ pages/models.py
@@ -91,7 +91,7 @@ class PostIndexPage(BasePage):
         if match_child := self.get_children().filter(slug=base).first():
             matched_route = match_child.specific.route(request, rest)
             return matched_route
-        if e := Entry.objects.filter(slug=rest[0]).first():
+        if e := Entry.objects.filter(slug=rest[0]).first() and not flag_is_active("v3"):
             return self, [], {"pk": e.pk}
         return super().route(request, path_components)
 
