306816f5ea899b030ae0897a177f12cffe98d208
javiercoronadonarvaez <javier.coronado@metalab.com>
2026-09-08 10:27:50 -0600

    feat: add update and remove buttons for post editing

--- news/views.py
+++ news/views.py
@@ -587,6 +587,7 @@ class V3AllTypesCreateView(V3Mixin, AllTypesCreateView):
         related_libraries: list,
         block_name: str,
         post_type: str,
+        remove_image: bool = False,
     ):
         cleaned_data = form.cleaned_data
         page.title = cleaned_data.get("title")
@@ -607,6 +608,8 @@ class V3AllTypesCreateView(V3Mixin, AllTypesCreateView):
                 file=image,
             )
             page.image = wagtail_image
+        elif remove_image:
+            page.image = None
         tags = []
         if related_libraries:
             for library in related_libraries:
@@ -671,6 +674,7 @@ class V3AllTypesCreateView(V3Mixin, AllTypesCreateView):
                         block_name=block_name,
                         post_type=post_type,
                         related_libraries=post_data.getlist("related_libraries"),
+                        remove_image=bool(post_data.get("remove_image")),
                     )
                 except Library.DoesNotExist:
                     return self.error_message_and_render(
@@ -861,6 +865,7 @@ class V3AllTypesEditView(V3AllTypesCreateView):
                     block_name=block_name,
                     post_type=post_type,
                     related_libraries=post_data.getlist("related_libraries"),
+                    remove_image=bool(post_data.get("remove_image")),
                 )
                 page.save_revision(user=request.user)
                 if not page.workflow_in_progress:
