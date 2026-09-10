96b4f0aa9cb2a6954b76f075ab41c53929e052c1
Jeremy Childers <jeremy@cppalliance.org>
2026-09-10 15:40:26 -0400

    Story 2518: Update legal page style

--- pages/blocks.py
+++ pages/blocks.py
@@ -1,6 +1,8 @@
 from wagtail.blocks import CharBlock
+from wagtail.blocks import ChoiceBlock
 from wagtail.blocks import RichTextBlock
 from wagtail.blocks import StreamBlock
+from wagtail.blocks import StructBlock
 from wagtail.blocks import URLBlock
 from wagtail.embeds.blocks import EmbedBlock
 from wagtailmarkdown.blocks import MarkdownBlock
@@ -24,3 +26,32 @@ POST_BLOCKS = [
     VIDEO_BLOCK,
     ("poll", PollBlock()),
 ]
+
+_icon_choices = [
+    "lightbulb",
+    "building-community",
+    "external-link",
+    "warning-box",
+    "repeat",
+    "user",
+    "close",
+    "mail",
+    "view-list",
+    "reload",
+    "cookie",
+    "chart-bar",
+    "question-mark",
+]
+
+
+class LegalPageCardBlock(StructBlock):
+    title = CharBlock(
+        required=True,
+    )
+    icon = ChoiceBlock(
+        choices=[(x, (" ").join(x.split("-")).title()) for x in _icon_choices],
+        required=True,
+    )
+    content = RichTextBlock(
+        required=True,
+    )
